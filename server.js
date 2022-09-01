ddc9fece02c1a3fce9a8696c558779fe57623454
root6439 <nicolas.soaresh.98@gmail.com>
2022-09-01 20:07:01 -0300

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
+const nomeApp = process.env.npm_package_name;
 const app = express();
-app.use(express.static(__dirname + "/dist/fundamentos-angular"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/fundamentos-angular/index.html"));
+
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
+
 app.listen(process.env.PORT || 8080);
